de72ede433a89707a0199bedd8a68f237c47dc8b
Shreyas Joshi <shreyas.joshi@aviatnet.com>
2015-10-12 17:09:01 +1300

    added event driven function for button press.

--- mainXCoreUtility.py
+++ mainXCoreUtility.py
@@ -2,11 +2,36 @@ import sys
 from PyQt4 import QtGui
 from coredumputilframe  import Ui_coreDumpUtilFrame
 
+def dbgLog(msg):
+    # while making the actual product, comment the below line and uncomment pass
+    print msg
+    #pass 
+
+
 class Main(QtGui.QMainWindow, Ui_coreDumpUtilFrame):
     def __init__(self):
        QtGui.QMainWindow.__init__(self)
        self.ui= Ui_coreDumpUtilFrame()
        self.ui.setupUi(self)
+       ## added this line to do event driven -- function to be called when the button is clicked 
+       self.ui.bSysRootBrowse.clicked.connect(self.ButtonbrowseSysRoot)
+       self.ui.bApplicationBrowser.clicked.connect(self.browseApplicationExe)
+       self.ui.lCoreFileBrowser.clicked.connect(self.browseCoreFile)
+  
+ ## browse button event installer ########
+  
+    def ButtonbrowseSysRoot(self):
+        dbgLog('browseSys Root pressed')
+  
+    def browseApplicationExe(self):
+        dbgLog('browse Application Exe')
+
+    def browseCoreFile(self):
+       dbgLog('browse core file')
+
+ #### browse button event installer completed ###########  
+
+ 
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
